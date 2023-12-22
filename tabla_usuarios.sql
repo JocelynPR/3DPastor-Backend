e95dac7e7385165dfe835ed885d51468e91f50c2
Jorge Ramirez <jorgermzc16@gmail.com>
2023-12-22 12:48:23 -0600

    tabla usuarios

---
+++ tabla_usuarios.sql
@@ -0,0 +1,21 @@
+insert into usuarios(usuario_nombre, email, telefono, contrasena, rol_id)
+  values ("Antonio González Navarrete", "navarrete.antonio.2301@gmail.com", "+52 3329467527", "pikachu69", 1);
+insert into usuarios(usuario_nombre, email, telefono, contrasena, rol_id)
+  values ("Mirta Adriana Tapia Don", "mirta.atd@gmail.com","+52 1 33 1531 6295", "charizard666", 1);
+insert into usuarios(usuario_nombre, email, telefono, contrasena, rol_id)
+  values ("Misael Edgar Zepeda Díaz", "misaeltup@gmail.com","+52 1 55 7209 7393", "mewtwo123", 1);
+insert into usuarios(usuario_nombre, email, telefono, contrasena, rol_id)
+  values ("Jocelyn Karina Peña Reyes", "jocelyn.p.reyes332b@gmail.com", "+52 5546935091", "snorlax99", 1);
+insert into usuarios(usuario_nombre, email, telefono, contrasena, rol_id)
+  values ("Yazmin Valeria Morales Martínez", "myaz5848@gmail.com", "5620893079", "bulbasuar567", 1);
+insert into usuarios(usuario_nombre, email, telefono, contrasena, rol_id)
+  values ("Jorge Alberto Ramirez Casillas", "jorgermzc16@gmail.com", "3311389817", "ivysur987", 1);
+
+insert into usuarios (usuario_nombre, email, telefono, contrasena, rol_id)
+  values ("Ana María Pérez", "anamaria@gmail.com", "+52 3334567890", "clave123", 2);
+insert into usuarios (usuario_nombre, email, telefono, contrasena, rol_id)
+  values ("Carlos González Rodriguez", "carlos.gonzalez@gmail.com", "+52 1 3356789012", "password456", 2);
+insert into usuarios (usuario_nombre, email, telefono, contrasena, direccion, rol_id)
+  values ("Laura Martínez Silva", "laura.martinez@outlook.com", "3389012345", "securepass789", 2);
+insert into usuarios (usuario_nombre, email, telefono, contrasena, direccion, rol_id)
+  values ("Jose Avila Camacho", "avila.jose@yahoo.com", "+52 3389012345", "securepass789", 2);
