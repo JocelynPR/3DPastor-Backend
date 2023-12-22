37dbad58912e7a31d22d781b40522a3807f103ed
Mirta Tapia <mirta.atd@gmail.com>
2023-12-22 15:21:16 -0600

    Terminé la vista de Lista de Usuarios y sus Compras

--- BD/Views/Lista de Usuarios y sus Compras.sql
+++ BD/Views/Lista de Usuarios y sus Compras.sql
@@ -1,17 +1,15 @@
--- CREATE VIEW `Lista de Usuarios y sus Compras` AS
+CREATE VIEW `Lista de Usuarios y sus Compras` AS
 SELECT
     u.usuario_id AS "ID",
     u.usuario_nombre AS "Nombre",
     c.compras_id AS "Id de Compra",
     c.fecha_compra AS "Fecha de Compra",
     ec.estado_compra AS "Estado",
-    
-    chp.cantidad_compra AS "Cantidad Comprada",
-    p.nombre_producto AS "Producto Comprado",
-    (chp.cantidad_compra * p.precio) AS "Subtotal"
+    SUM( (chp.cantidad_compra * p.precio) ) AS "Total"
 FROM usuarios u
 NATURAL JOIN compras c
 NATURAL JOIN estado_compra ec
 NATURAL JOIN compras_has_productos chp
 INNER JOIN productos p
-ON chp.productos_id = p.producto_id;
+ON chp.productos_id = p.producto_id
+GROUP BY `Id de Compra`;
