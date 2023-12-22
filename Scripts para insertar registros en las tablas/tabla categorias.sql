fbfba20dad0521ad1beb0d3b8286e7b3a2322537
misael-tup <misabul@comunidad.unam.edu>
2023-12-22 12:53:43 -0600

    Actualizando

---
+++ Scripts para insertar registros en las tablas/tabla categorias.sql
@@ -0,0 +1,19 @@
+SELECT * FROM pokemon_place.categorias;
+
+INSERT INTO categorias (categoria_id, nombre_categoria) 
+VALUES ( 1, "Productos oficial");
+
+INSERT INTO categorias (categoria_id, nombre_categoria) 
+VALUES ( 2, "Accesorios");
+
+INSERT INTO categorias (categoria_id, nombre_categoria) 
+VALUES ( 3, "Juguetes");
+
+INSERT INTO categorias (categoria_id, nombre_categoria) 
+VALUES ( 4, "Postres");
+
+INSERT INTO categorias (categoria_id, nombre_categoria) 
+VALUES ( 5, "Juegos de Mesa");
+
+INSERT INTO categorias (categoria_id, nombre_categoria) 
+VALUES ( 6, "Otros");
---
+++ Scripts para insertar registros en las tablas/tabla productos.sql
@@ -0,0 +1,249 @@
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Sticker de Vinil Transparente de Wooloo", 15, 6, 0 );
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Sticker de Vinil Transparente de Charizard", 15, 6, 0 );
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Sticker de Vinil Holográfico de Gardevoir", 15, 6, 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Sticker de Vinil Transparente de Chandelure", 15, 6, 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Squishy Chillón de Charmander", 40, 3, 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Squishy Chillón de Snorlax", 40, 3, 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Sticker de Vinil Transparente de Detective Pikachu", 15, 6, 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Sticker de Vinil Transparente de Gengar", 15, 6, 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Pokémon Fluxx", 350, 5, 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Pokémon Coup", 250, 5, 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Pokélate", 100, 4, 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Pokélate Master Ball", 150, 4 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Pokélate Love Ball", 150, 4 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Peluche de Kyogre", 300, 3 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Peluche de Psyduck", 300, 3 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Sticker de Vinil Transparente de Rowlet", 15, 6 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Sticker de Vinil Transparente de 9° Aniversario", 15, 6 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Sobre de 3 League Cards Series 3 - Starters de Hoenn", 20, 6 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Sobre de 3 League Cards Series 2 - Pikachu Clones", 20, 6 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Sobre de 3 League Cards Series 1 - Starters de Galar", 20, 6 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Sobre de 3 League Cards Series 4 - Top 30 Pokémon de Kanto de 2020", 20, 6 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Squishy Chillón de Bulbasaur", 40, 3 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Llavero de Psyduck", 30, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Collar de Eevee Gigantamax", 45, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Figura de Resina de Eevee (Impresión 3D)", 80, 3 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Figura de Resina de Pokébola de Pikachu (Impresión 3D)", 80, 3 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Llavero de Squirtle", 30, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Collar de Pikachu", 40, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Figura de Resina de Pokébola de Charmander (Impresión 3D)", 80, 3 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Figura de Resina de Pokébola de Squirtle (Impresión 3D)", 80, 3 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Llavero de Ditto", 30, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Charm de Bulbasaur", 10, 3 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Charm de Psyduck", 10, 3 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Figura de Resina de Squirtle (Impresión 3D)", 80, 3 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Charm de Pikachu", 10, 3 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Llavero de Pokébola", 30, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Llavero de Ho-Oh", 30, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Llavero de Pikachu", 30, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Llavero de Snorlax", 30, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Collar de Ditto", 40, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Figura de Resina de Pokébola de Eevee (Impresión 3D)", 80, 3 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Llavero de Pikachu", 30, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Figura de Resina de Pokébola de Bulbasaur (Impresión 3D)", 80, 3 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Figura que abraza tu cable cargador de celular de Vulpix", 45, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Llavero de Raikou", 30, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Llavero de Togepi", 30, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Llavero de Jigglypuff", 30, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Llavero de Jigglypuff", 30, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Llavero de Squirtle", 30, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Figura de Resina de Bulbasaur (Impresión 3D)", 80, 3 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Llavero de Pikachu", 30, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Llavero de Pokébola Tejida a Mano", 50, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Llavero de Bulbasaur", 30, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Pluma de Pikachu", 35, 6 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Collar de Pikachu", 80, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Llavero Cascabel de Great Ball", 30, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Figura de Resina de Charmander (Impresión 3D)", 80, 3 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Figura que abraza tu cable cargador de celular de Meowth", 45, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Llavero de Entei", 30, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Gancho de Leafeon (Pégalo en la pared y dobla su colita para colgar cosas en ella)", 100, 6 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Figura que abraza tu cable cargador de celular de Torchic", 45, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Llavero de Lapras Gigantamax", 45, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Llavero de Squirtle", 30, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Llavero de Slowpoke", 30, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Llavero de Bulbasaur", 30, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Llavero de Bulbasaur", 30, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Llavero de Eevee", 30, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Llavero de Charmander", 30, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Llavero de Charmander", 30, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Llavero de Charmander", 30, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Llavero de Meowth", 30, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Llavero de Bulbasaur", 30, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Llavero de Psyduck", 30, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Llavero de Squirtle", 30, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Figura que abraza tu cable cargador de celular de Piplup", 45, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Llavero de Charmander", 30, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Bolsa Pequeña de Eevee", 80, 6 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Pulsera de Charmander", 80, 2 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Paquete de Energías de Pokémon TCG / JCC", 50, 1 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Micas para cartas de TCG / JCC de la Expansión 151", 200, 1 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Dados de TCG / JCC de la Expansión 151", 100, 1 , 0);
+
+INSERT INTO productos (nombre_producto, precio, categoria_id,  cantidad_disponible) 
+VALUES ("Marcadores de TCG / JCC de Veneno y Quemadura", 100, 1 , 0);
+
+
+
